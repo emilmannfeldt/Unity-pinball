128186901fb611bc5d45a1b62a16d241863ed0df
Emil mannfeldt <emima980@student.liu.se>
2015-04-05 15:37:16 +0200

    first commit

---
+++ Assets/flipperScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flipperScript : MonoBehaviour {
+
+    public Rigidbody2D rd;
+    public HingeJoint2D hj;
+    public JointMotor2D motor;
+    
+    public float torque;
+    public float speed;
+	void Start() {
+        
+        motor = hj.motor;
+        motor.motorSpeed = speed;
+        motor.maxMotorTorque = torque;
+        hj.motor = motor;
+        hj.useMotor = false;
+	}
+	void Update() {
+        
+        if (Input.GetMouseButtonDown(0))
+        {
+           
+
+            
+            hj.useMotor = true;
+          
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            hj.useMotor = false;
+        }
+       
+	
+	}
+
+}
---
+++ Assets/flipperscript2.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flipperscript2 : MonoBehaviour {
+
+    public Rigidbody2D rd;
+    public HingeJoint2D hj;
+    public JointMotor2D motor;
+
+    public float torque;
+    public float speed;
+    void Start()
+    {
+
+        motor = hj.motor;
+        motor.motorSpeed = speed;
+        motor.maxMotorTorque = torque;
+        hj.motor = motor;
+        hj.useMotor = false;
+        
+    }
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            
+
+            hj.useMotor = true;
+
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            hj.useMotor = false;
+        }
+
+
+    }
+}
